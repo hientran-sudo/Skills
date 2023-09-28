40afbd081e075504050e851e4d1c59859a15a655
hientran-sudo <hien.tran@student.fairfield.edu>
2023-09-28 12:53:22 -0400

    add Shared module + product 1 to list

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -1,9 +1,15 @@
+import { NgIf } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-product',
-    template: '<h2>Product List</h2>'
+    template: `<h2>Product List</h2>
+               <div>Product 1</div>`,
+    styles: ['div {font-weight:bolder; color:darkgreen;}']
+   
 })
+
 export class ProductComponent{
-    
+    //showDiv=true;
+
 }
