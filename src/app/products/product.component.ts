cca692d73a300108b8b5b5880fea490158dda0a1
hientran-sudo <hien.tran@student.fairfield.edu>
2023-09-28 15:36:39 -0400

    ngFor iterate through an array

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -4,15 +4,18 @@ declare const genRandomNumbers:any;
 
 @Component({
     selector: 'app-product',
-    template: `<h2>Product List</h2>
-               <div>Product 1</div>
-               <div>{{rNum}}</div>`,
-    styles: ['div {font-weight:bolder; color:darkgreen;}']
+    template: //`<h2>Product List</h2>
+              // <div>Product 1</div>
+              // <div>{{rNum}}</div>`
+              '<li *ngFor="let dept of departments">{{dept}}</li>',
+    styles: //['div {font-weight:bolder; color:darkgreen;}']
+              ['li {color: purple; margin-top: 10px; font-size: 1.5emA}']
    
 })
 
 export class ProductComponent{
     //showDiv=true;
     rNum = <[]>genRandomNumbers();
+    departments = ["Bicycles","Car","Boat"];
 
 }
