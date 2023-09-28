82b394ded3cbb0180dd5d8336f81f43f92bea82e
hientran-sudo <hien.tran@student.fairfield.edu>
2023-09-28 15:44:01 -0400

    employee objects

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -7,7 +7,8 @@ declare const genRandomNumbers:any;
     template: //`<h2>Product List</h2>
               // <div>Product 1</div>
               // <div>{{rNum}}</div>`
-              '<li *ngFor="let dept of departments">{{dept}}</li>',
+              //'<li *ngFor="let dept of departments">{{dept}}</li>',
+                '<li *ngFor="let employee of employees">{{employee.name}}</li>',
     styles: //['div {font-weight:bolder; color:darkgreen;}']
               ['li {color: purple; margin-top: 10px; font-size: 1.5emA}']
    
@@ -17,5 +18,22 @@ export class ProductComponent{
     //showDiv=true;
     rNum = <[]>genRandomNumbers();
     departments = ["Bicycles","Car","Boat"];
+    employees = [
+        {
+            name: "Alex",
+            employeeID: 1234,
+            department: "IT"
+        },
+        {
+            name: "Peter",
+            employeeID: 1234,
+            department: "IT"
+        },
+        {
+            name: "Mike",
+            employeeID: 1234,
+            department: "IT"
+        }
 
+    ];
 }
