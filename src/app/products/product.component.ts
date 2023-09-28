350738a4002585946d9343eb18e0c61212ff7b6c
hientran-sudo <hien.tran@student.fairfield.edu>
2023-09-28 13:07:14 -0400

    add js file to component

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -1,15 +1,18 @@
 import { NgIf } from "@angular/common";
 import { Component } from "@angular/core";
+declare const genRandomNumbers:any;
 
 @Component({
     selector: 'app-product',
     template: `<h2>Product List</h2>
-               <div>Product 1</div>`,
+               <div>Product 1</div>
+               <div>{{rNum}}</div>`,
     styles: ['div {font-weight:bolder; color:darkgreen;}']
    
 })
 
 export class ProductComponent{
     //showDiv=true;
+    rNum = <[]>genRandomNumbers();
 
 }
