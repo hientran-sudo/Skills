78d3d775227f08360d283e87b39c37990509a01c
hientran-sudo <hien.tran@student.fairfield.edu>
2023-09-29 08:42:19 -0400

    ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
+import { ProductComponent } from './products/product.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Skills';
-}
+  // -------VIEWCHILD
+  quantity:number = 0;
+  @ViewChild(ProductComponent) product: ProductComponent;
+  ngAfterViewInit(){
+    this.product.childMethod();
+    this.quantity=this.product.instock;
+    console.log(this.quantity);
+  }
+  // -------PASS DATA FROM CHILD TO PARENT
+  //p_addProduct(newProduct:string){
+  //  console.log(newProduct);
+
+  }
+
--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -1,32 +1,48 @@
 import { NgIf } from "@angular/common";
-import { Component, Input } from "@angular/core";
+//import { Component, Input } from "@angular/core";
+//import { Component, Output, EventEmitter } from "@angular/core";
+import { Component } from "@angular/core";
 declare const genRandomNumbers:any;
 
 @Component({
     selector: 'app-product',
-    template: `<h2>Product List</h2>
-               <div>{{p_title}}</div>`,
-              // <div>{{rNum}}</div>`,
-              //----------------------
-              //`<h2>Product List</h2>
-              //<div *ngFor="let x of rNum | paginate: {itemsPerPage: itemsToDisplay, currentPage:page}">
-              //{{x | classify}}</div>
-              //<pagination-controls (pageChange)="pageChanged($event)"></pagination-controls>`,
-              //'<li *ngFor="let dept of departments">{{dept}}</li>',
-              //'<li *ngFor="let employee of employees">{{employee.name}}</li>',
+    template:`<h2>Product List</h2>`,
+            //<label>Add new product
+            //<input #newProduct></label>
+            //<button (click) ="addProduct(newProduct.value)">Add</button>`,
+              
+            //<div>{{p_title}}</div>`,
+            //<div>{{rNum}}</div>`,
+            //----------------------
+            //`<h2>Product List</h2>
+            //<div *ngFor="let x of rNum | paginate: {itemsPerPage: itemsToDisplay, currentPage:page}">
+            //{{x | classify}}</div>
+            //<pagination-controls (pageChange)="pageChanged($event)"></pagination-controls>`,
+            //'<li *ngFor="let dept of departments">{{dept}}</li>',
+            //'<li *ngFor="let employee of employees">{{employee.name}}</li>',
     styles: ['div {font-weight:bolder; color:darkgreen;}']
             //['li {color: purple; margin-top: 10px; font-size: 1.5emA}']
    
 })
 
 export class ProductComponent{
-    // NESTED COMPONENT
-    @Input() p_title:string | undefined;
+    // -------VIEWCHILD
+    instock:number = 10;
+    childMethod() {
+        console.log('A method in the Product Component - the child');
+    }
+    // -------PASS DATA FROM CHILD TO PARENT
+    //@Output() c_newProductEvent = new EventEmitter<string>();
+    //addProduct(value:string){
+    //    this.c_newProductEvent.emit(value);
+    //}
+    // -------NESTED COMPONENT
+    //@Input() p_title:string | undefined;
     // SORT
     //rNum = <[]>genRandomNumbers().sort(function(x:number,y:number){
     //    return x-y
     //});
-    // FILTER 
+    // -------FILTER 
     //rNum = <[]>genRandomNumbers().filter((x:number) => x < 2000);
     //page: number = 1;
     //itemsToDisplay: number = 10;
@@ -35,7 +51,7 @@ export class ProductComponent{
     //    this.page = event;
 //
     //}
-    // ARRAY AND OBJECT
+    // -------ARRAY AND OBJECT
     //departments = ["Bicycles","Car","Boat"];
     //employees = [
     //    {
