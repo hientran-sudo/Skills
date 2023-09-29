7d7b15b347841c9ac7aa02f4663155d4ba1527e9
hientran-sudo <hien.tran@student.fairfield.edu>
2023-09-29 08:04:23 -0400

    nested component

--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -1,16 +1,17 @@
 import { NgIf } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 declare const genRandomNumbers:any;
 
 @Component({
     selector: 'app-product',
-    template: //`<h2>Product List</h2>
-              // <div>Product 1</div>
+    template: `<h2>Product List</h2>
+               <div>{{p_title}}</div>`,
               // <div>{{rNum}}</div>`,
-               `<h2>Product List</h2>
-               <div *ngFor="let x of rNum | paginate: {itemsPerPage: itemsToDisplay, currentPage:page}">
-               {{x | classify}}</div>
-               <pagination-controls (pageChange)="pageChanged($event)"></pagination-controls>`,
+              //----------------------
+              //`<h2>Product List</h2>
+              //<div *ngFor="let x of rNum | paginate: {itemsPerPage: itemsToDisplay, currentPage:page}">
+              //{{x | classify}}</div>
+              //<pagination-controls (pageChange)="pageChanged($event)"></pagination-controls>`,
               //'<li *ngFor="let dept of departments">{{dept}}</li>',
               //'<li *ngFor="let employee of employees">{{employee.name}}</li>',
     styles: ['div {font-weight:bolder; color:darkgreen;}']
@@ -19,18 +20,22 @@ declare const genRandomNumbers:any;
 })
 
 export class ProductComponent{
-    //showDiv=true;
+    // NESTED COMPONENT
+    @Input() p_title:string | undefined;
+    // SORT
     //rNum = <[]>genRandomNumbers().sort(function(x:number,y:number){
     //    return x-y
     //});
-    rNum = <[]>genRandomNumbers().filter((x:number) => x < 2000);
-    page: number = 1;
-    itemsToDisplay: number = 10;
+    // FILTER 
+    //rNum = <[]>genRandomNumbers().filter((x:number) => x < 2000);
+    //page: number = 1;
+    //itemsToDisplay: number = 10;
 
-    pageChanged(event:any){
-        this.page = event;
-
-    }
+    //pageChanged(event:any){
+    //    this.page = event;
+//
+    //}
+    // ARRAY AND OBJECT
     //departments = ["Bicycles","Car","Boat"];
     //employees = [
     //    {
