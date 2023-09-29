dbe8bb6d9ed7301035a103de2b92cd129fbf54b4
hientran-sudo <hien.tran@student.fairfield.edu>
2023-09-29 05:50:55 -0400

    pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-//import { ProductComponent } from './products/product.component';
-import { ProductsFeatureModule } from './products-feature/products-feature.module';
+import { ProductComponent } from './products/product.component';
+//import { ProductsFeatureModule } from './products-feature/products-feature.module';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    //ProductComponent
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProductsFeatureModule
+    //ProductsFeatureModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -6,34 +6,44 @@ declare const genRandomNumbers:any;
     selector: 'app-product',
     template: //`<h2>Product List</h2>
               // <div>Product 1</div>
-              // <div>{{rNum}}</div>`
+              // <div>{{rNum}}</div>`,
+               `<h2>Product List</h2>
+               <div *ngFor="let x of rNum | paginate: {itemsPerPage: itemsToDisplay, currentPage:page}">
+               {{x}}</div>
+               <pagination-controls (pageChange)="pageChanged($event)"></pagination-controls>`,
               //'<li *ngFor="let dept of departments">{{dept}}</li>',
-                '<li *ngFor="let employee of employees">{{employee.name}}</li>',
-    styles: //['div {font-weight:bolder; color:darkgreen;}']
-              ['li {color: purple; margin-top: 10px; font-size: 1.5emA}']
+              //'<li *ngFor="let employee of employees">{{employee.name}}</li>',
+    styles: ['div {font-weight:bolder; color:darkgreen;}']
+            //['li {color: purple; margin-top: 10px; font-size: 1.5emA}']
    
 })
 
 export class ProductComponent{
     //showDiv=true;
     rNum = <[]>genRandomNumbers();
-    departments = ["Bicycles","Car","Boat"];
-    employees = [
-        {
-            name: "Alex",
-            employeeID: 1234,
-            department: "IT"
-        },
-        {
-            name: "Peter",
-            employeeID: 1234,
-            department: "IT"
-        },
-        {
-            name: "Mike",
-            employeeID: 1234,
-            department: "IT"
-        }
+    page: number = 1;
+    itemsToDisplay: number = 10;
 
-    ];
+    pageChanged(event:any){
+        this.page = event;
+
+    }
+    //departments = ["Bicycles","Car","Boat"];
+    //employees = [
+    //    {
+    //        name: "Alex",
+    //        employeeID: 1234,
+    //        department: "IT"
+    //    },
+    //    {
+    //        name: "Peter",
+    //        employeeID: 1234,
+    //        department: "IT"
+    //    },
+    //    {
+    //        name: "Mike",
+    //        employeeID: 1234,
+    //        department: "IT"
+    //    }
+    //];
 }
