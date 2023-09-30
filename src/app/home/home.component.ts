de4a05a84bd7054b974c00b55e7bbdf296ed706e
hientran-sudo <hien.tran@student.fairfield.edu>
2023-09-30 17:29:58 -0400

    http post request

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,10 +11,11 @@ export class HomeComponent implements OnInit {
   email: string;
   password: string;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   onSubmit(form: NgForm){
-    console.log(form.value);
+    //console.log(form.value);
+    this.http.post("http://localhost:3000/customers", form.value).subscribe();
   }
 
   ngOnInit(): void {
