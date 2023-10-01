731d737be1b20da5e6af15a4663aaf49d72ca7b3
hientran-sudo <hien.tran@student.fairfield.edu>
2023-10-01 16:03:40 -0400

    authorization

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthorizationService } from './authorization.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthorizationService){
+
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
+    if (!this.authService.isAuthorized())
     return false;
+    else 
+    return true;
+
   }  
 }
