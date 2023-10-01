abddc667b2a166ce91aae188115140cc630e29c8
hientran-sudo <hien.tran@student.fairfield.edu>
2023-10-01 16:57:01 -0400

    sauth

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,19 +3,23 @@ import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } fro
 import { Observable } from 'rxjs';
 import { AuthorizationService } from './authorization.service';
 import { ProfileService } from './profile.service';
+import { SAuthService } from './s-auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   //constructor(private authService: AuthorizationService){
-    constructor(private profile: ProfileService){
+  //constructor(private profile: ProfileService){
+    constructor(private authService: SAuthService){}
 
-  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): boolean {
+      if (!this.authService.getToken())
       return false;
+      else
+      return true;
       //if (!this.profile.loggedInStatus)
       //  return false;
       //else 
